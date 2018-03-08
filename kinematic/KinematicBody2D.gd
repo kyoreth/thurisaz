e0a0285e1456ee65edb77e5e9909b1aee0da4559
Zenthirum <zenthirum@protonmail.com>
2018-03-08 22:10:12 +0100

    Variable jumping

--- kinematic/KinematicBody2D.gd
+++ kinematic/KinematicBody2D.gd
@@ -2,19 +2,36 @@ extends KinematicBody2D
 
 const GRAVITY = 500.0
 const WALK_SPEED = 150
-const JUMP_SPEED = 350
+const JUMP_SPEED = 50
+const MAX_JUMP_SPEED = 400
+
+# States
+const IDLE = 0
+const JUMPING = 1
+const FALLING = 2
+
 var acceleration = Vector2()
 var velocity = Vector2()
+var target_velocity = Vector2()
 var airborne = false
 var walk_direction = "none"
+var state = IDLE
+var timeElapsed = 0
 
 func _physics_process(delta):
 	airborne = not is_on_floor()
-	acceleration.y += delta * GRAVITY
-	velocity.y = acceleration.y # don't need to take delta for velocity, 
-								# it's already built-into move_and_slide
+	
+	if velocity.y > 0:
+		state = FALLING
+	
+	# stop going upwards if hitting a ceiling
+	if is_on_ceiling():
+		velocity.y = 0
 	
 	if not airborne:
+		state = IDLE
+		velocity.y = 0 # reset velocity effects from gravity
+		
 		if Input.is_action_pressed("ui_left"):
 			walk_direction = "left"
 			velocity.x = -WALK_SPEED
@@ -24,11 +41,10 @@ func _physics_process(delta):
 		else:
 			walk_direction = "none"
 			velocity.x = 0
-		
-		if Input.is_action_just_pressed("ui_space"):
-				acceleration.y = -JUMP_SPEED
 	
 	if airborne:
+		velocity.y += delta * GRAVITY
+		
 		if Input.is_action_pressed("ui_left") and not walk_direction == "right":
 			walk_direction = "left"
 			velocity.x = -WALK_SPEED
@@ -36,4 +52,25 @@ func _physics_process(delta):
 			walk_direction = "right"
 			velocity.x = WALK_SPEED
 	
+	if Input.is_action_just_pressed("ui_space"):
+		if not state == JUMPING and not state == FALLING:
+			state = JUMPING
+#			print("Pressed!")
+	
+	if state == JUMPING:
+		if Input.is_action_pressed("ui_space") and not is_on_ceiling():
+			velocity.y += -JUMP_SPEED
+			if velocity.y <= -MAX_JUMP_SPEED:
+				velocity.y = -MAX_JUMP_SPEED
+				state = FALLING
+			
 	move_and_slide(velocity, Vector2(0,-1))
+	
+	if Input.is_action_just_pressed("ui_space"):
+#	timeElapsed += delta
+#	if timeElapsed >= 0.5:
+		print("Velocity = ", velocity)
+		print("Airborne: ", airborne)
+		print("On ceiling: ", is_on_ceiling())
+		print("State: ", state)
+#		timeElapsed -= 0.5
